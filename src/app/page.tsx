6653081be5ebf2f6339fc8d07da3801eae6b6c18
Cipher Professor <mohsinmanzoor1913@gmail.com>
2025-01-18 21:39:36 +0530

    Initial Commit Basic Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,87 @@
+"use client"
+import { SidebarDemo } from "@/components/ui/Aceternity/SideMenu/SidebarDemo";
+import Image from "next/image";
+import {Button} from '@heroui/button'; 
+import HeroUICalendar from "@/components/ui/HeroUI/Calendar/Calendar";
+import DateInputToFrom from "@/components/ui/HeroUI/DateInput/DateInputToFrom";
+import HeroUIDatePickerFromTo from "@/components/ui/HeroUI/DatePicker/DatePicker";
+import HeroUIDateRangePicker from "@/components/ui/HeroUI/DateRangePicker/DateRangePicker";
+import HeroUIForm from "@/components/ui/HeroUI/Form/HeroForm";
+import HeroUIListTextIconsNumber from "@/components/ui/HeroUI/ListBox/ListTextIconsNumber";
+import HeroUIListAvtarSelection from "@/components/ui/HeroUI/ListBox/ListAvtarSelection";
+import HeroUIDropDown2Select from "@/components/ui/HeroUI/DropDownSelect/DropDown2Select/DropDown2Select";
+import HeroUiDropDownAssignTo from "@/components/ui/HeroUI/DropDownSelect/DropDown2Select/DropDownAssignTo";
+import HeroUISwitchDarkLightModeIcon from "@/components/ui/HeroUI/Switch/SwitchDarkLightModeIcon";
+import HeroUIModernTableSearch from "@/components/ui/HeroUI/Table/ModernTableSearch";
+import HeroUITabWithForm from "@/components/ui/HeroUI/Tabs/TabWithForm";
+import HeroUIJustTabs from "@/components/ui/HeroUI/Tabs/JustTabs";
+import HeroUITextDescription from "@/components/ui/HeroUI/TextArea/Description";
+import HeroUITimeInput from "@/components/ui/HeroUI/TimeInput/TimeInput";
+import HeroUIColoursTooltip from "@/components/ui/HeroUI/Tooltip/ColorsTooltip";
+import HeroUIUsername from "@/components/ui/HeroUI/User/UserName";
+import { AcUIAnimatedTooltipPreview } from "@/components/ui/Aceternity/AnimatedTooltip/AnimatedTooltipPreview";
+import { AcUIFeaturesSectionDemo } from "@/components/ui/Aceternity/FeatureSection/FeatureGridText/FeatureGridText";
+import { AcUILayoutGridDemo } from "@/components/ui/Aceternity/LayoutGrid/LayoutGridDemo";
+import { AcUIMeteorsDemo } from "@/components/ui/Aceternity/Meteors/MeteorsDemo";
+import { AcUISidebarDemoUse } from "@/components/ui/Aceternity/SideBar/SidebarUse";
+
+import { AcUiStickyScrollRevealDemo } from "@/components/ui/Aceternity/StickyScroll/StickyScroll";
+import { AcUITimelineDemo } from "@/components/ui/Aceternity/TimeLine/TimeLineDemo";
+import { AcUITracingBeamDemo } from "@/components/ui/Aceternity/TrackingDemo/TrackingDemo";
+import { AcUIWorldMapDemo } from "@/components/ui/Aceternity/WorldMap/WorldMap";
+import { EldoraUIAnimatedCardDemo } from "@/components/ui/eldoraui/AnimatedCard/AnimatedCardDemo";
+// import { AceternityUIAnimatedTestimonialsUse } from "@/components/ui/Aceternity/AnimatedTestimonials/AnimatedTestimonialsUse";
+
+// import { AceternityUIAnimatedTestimonialsDemo } from "@/components/ui/Aceternity/AnimatedTestimonials/AnimatedTestimonialsUSe";
+
+export default function Home() {
+  return (
+    // <div >
+    //   Welcome To Next JS Repo with MongoDB
+     
+      
+    // </div>
+    <>
+    {/* <SidebarDemo /> */}
+
+    {/* <Button>Click me</Button> */}
+    {/* <HeroUICalendar /> */}
+    {/* <DateInputToFrom /> */}
+    {/* <HeroUIDatePickerFromTo /> */}
+    {/* <HeroUIDateRangePicker /> */}
+    {/* <HeroUIForm /> */}
+    {/* <HeroUIListTextIconsNumber /> */}
+    {/* <HeroUIListAvtarSelection /> */}
+    {/* <HeroUIDropDown2Select />  */}
+    {/* <HeroUiDropDownAssignTo /> */}
+    {/* <HeroUISwitchDarkLightModeIcon /> */}
+    <HeroUIModernTableSearch />
+    {/* <HeroUITabWithForm /> */}
+    {/* <HeroUIJustTabs/> */}
+    {/* <HeroUITextDescription /> */}
+    {/* <HeroUITimeInput/> */}
+    {/* <HeroUIColoursTooltip /> */}
+    {/* <HeroUIUsername /> */}
+    {/* <AceternityUIAnimatedTestimonialsUse /> */}
+    <div className="flex flex-row items-center justify-center mt-20 w-full pd-20">
+      
+    {/* <AcUIAnimatedTooltipPreview/> */}
+
+    {/* <AcUIFeaturesSectionDemo/> */}
+
+    {/* <AcUILayoutGridDemo/> */}
+    {/* <AcUIMeteorsDemo/> */}
+    {/* <AcUISidebarDemo/> */}
+    {/* <AcUISidebarDemoUse/> */}
+    {/* <AcUiStickyScrollRevealDemo/> */}
+    {/* <AcUITimelineDemo/> */}
+    {/* <AcUITracingBeamDemo/> */}
+    {/* <AcUIWorldMapDemo/> */}
+    {/* <EldoraUIAnimatedCardDemo /> */}
+   
+
+    </div>
+    </>
+    
+  );
+}
