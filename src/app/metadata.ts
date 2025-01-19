dc17a3841140ad743ea7d06b1543bb6e30a77663
Cipher Professor <mohsinmanzoor1913@gmail.com>
2025-01-20 01:28:57 +0530

    Logo

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,6 +1,15 @@
+"use client"
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "Gate Pass Management System",
   description: "A simple gate pass management system built with Next.js and HeroUI.",
+  icons: {
+    icon: [
+      { url: '/wclogo.png' },
+      { url: '/wclogo.ico' },
+    ],
+    apple: '/wclogo.png',
+    shortcut: '/wclogo.png',
+  },
 }
