11a23efff52cd6f3f189e1456acc3bea1a53a5de
Cipher Professor <mohsinmanzoor1913@gmail.com>
2025-01-19 16:30:31 +0530

    unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
           </SignedIn>
           
         <div className="min-h-screen bg-gray-50 flex flex-col">
-          <Header onMenuClick={() => setSidebarOpen(!sidebarOpen)} totalEarnings={totalEarnings} />
+          <Header onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
           <div className="flex flex-1">
             <Sidebar open={sidebarOpen} />
             <main className="flex-1 p-4 lg:p-8 ml-0 lg:ml-64 transition-all duration-300">
